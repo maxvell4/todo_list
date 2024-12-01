4a684237116b7cc86541700c75e92b582b0cbfa3
maxvell4 <tarasyukmaxim@gmail.com>
2024-12-01 15:20:13 +0200

    add models

---
+++ todo_list_service/forms.py
@@ -0,0 +1,13 @@
+from django import forms
+from .models import Task, Tag
+
+class TaskForm(forms.ModelForm):
+    model = Task
+    class Meta:
+        fields = ["content", "deadline", "status", "tags"]
+
+
+class TagForm(forms.ModelForm):
+    class Meta:
+        model = Tag
+        fields = ["Name"]
